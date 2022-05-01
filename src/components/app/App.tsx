3654b6f7b19295a899d0bc46f45629c6953be88c
Maryna Manzhula <mmanzhula@evolution.com>
2022-05-01 20:22:13 +0300

    Add title text

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,11 @@
 import { Overlay3D } from '../overlay3D/Overay3D';
+import Title from '../title/Title';
 import './App.pcss';
 
 export const App = () => {
     return (
-        <div className="App">
+        <div className="app">
+            <Title />
             <Overlay3D />
         </div>
     );
---
+++ src/components/title/Title.tsx
@@ -0,0 +1,7 @@
+import './Title.pcss';
+
+export const Title = () => {
+    return (<h1 className="title">Welcome to</h1>);
+}
+
+export default Title;
